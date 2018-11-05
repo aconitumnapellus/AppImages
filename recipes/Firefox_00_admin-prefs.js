29a55dd2b7bcf31f7d798261dfcf8256b2b7b335
no <no@you.at>
2018-11-05 16:04:55 +0100

    fixxes

--- recipes/Firefox_00_admin-prefs.js
+++ recipes/Firefox_00_admin-prefs.js
@@ -2,3 +2,4 @@
 // https://developer.mozilla.org/en-US/Firefox/Enterprise_deployment_before_60
 pref("general.config.filename", "mozilla.cfg");
 pref("general.config.obscure_value", 0);
+pref("plugins.load_appdir_plugins", true);
