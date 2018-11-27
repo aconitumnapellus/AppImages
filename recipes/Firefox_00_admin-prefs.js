f78a0f0a7c4b4e2d1cc11bdf0b14529859f10084
no <no@you.at>
2018-11-27 11:35:35 +0100

    getting back to main

--- recipes/Firefox_00_admin-prefs.js
+++ recipes/Firefox_00_admin-prefs.js
@@ -2,4 +2,3 @@
 // https://developer.mozilla.org/en-US/Firefox/Enterprise_deployment_before_60
 pref("general.config.filename", "mozilla.cfg");
 pref("general.config.obscure_value", 0);
-pref("plugins.load_appdir_plugins", true);
